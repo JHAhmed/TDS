b655cbc6ba1187c7da2233b25f39717d457fb07e
JHAhmed <jamalhascientist@gmail.com>
2026-07-07 05:27:56 +0530

    TDS: add retry header

--- test1.py
+++ test1.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Retry-After"],  # <-- THIS IS THE MAGIC LINE
 )
 
 lock = threading.Lock()
